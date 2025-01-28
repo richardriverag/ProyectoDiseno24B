1bca7ed71d3773bfe8f657c5f4a9bb47a040a019
Tatiananay <nayeli.leiva@epn.edu.ec>
2025-01-28 15:31:27 -0500

    Modify(Usuario): change view frmMenuBar and controllers are added

---
+++ src/main/java/controlador/usuarios/CtrUsuario.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador.usuarios;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.frmMenuBar;
+
+/**
+ *
+ * @author LENOVO.USER
+ */
+public class CtrUsuario implements ActionListener{
+    private frmMenuBar frmConP;
+
+    public CtrUsuario(frmMenuBar frmContenedorPrincipal) {
+        this.frmConP = frmContenedorPrincipal;
+        this.frmConP.jMenuIUGuardar.addActionListener(this);
+        this.frmConP.jMenuIUBuscar.addActionListener(this);
+        this.frmConP.jMenuIUModificar.addActionListener(this);
+        this.frmConP.jMenuIUEliminar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        CardLayout cardLayout = (CardLayout) frmConP.PanelPrincipal.getLayout();
+        if (e.getSource() == frmConP.jMenuIUGuardar) {
+            cardLayout.show(frmConP.PanelPrincipal, "card3");  // PanelGuardar
+        }
+        if (e.getSource() == frmConP.jMenuIUBuscar) {
+            cardLayout.show(frmConP.PanelPrincipal, "card2");  // PanelBuscar
+        }
+        if (e.getSource() == frmConP.jMenuIUModificar) {
+            cardLayout.show(frmConP.PanelPrincipal, "card5");  // PanelModificar
+        }
+        if (e.getSource() == frmConP.jMenuIUEliminar) {
+            cardLayout.show(frmConP.PanelPrincipal, "card6");  // PanelEliminar
+        }
+    }
+}
